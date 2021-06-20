a1c16727a19104cdcd1c9c504f0181d293bfb841
patrik <patrik.lindstrom@lcube.se>
2021-06-20 18:47:48 +0000

    f/add-clapper (#1)
    union subcommand for clap works
    Co-Authored-By: patrik <patrik.lindstrom@lcube.se>
    Co-Committed-By: patrik <patrik.lindstrom@lcube.se>

---
+++ readme.md
@@ -0,0 +1,63 @@
+**tl;dr This cmd program can compare textfiles and find what rows are the
+same or differ**
+
+# Readme zet-cmd
+
+# How to use it
+## What problem does it solve?
+When you have data in two or more files and want to extract parts of if. In
+similar way that you would from a sql database. It could be only the unique
+rows in several files or the differences between two files or what has been
+added to a file compared to previous version.
+
+## Why not use a sql database ?
+Absolutley if you have sql database use that. But if you do not have access to
+a database or this is something that is just something you have to do once it
+could be simpler solution. It can also be part of an ETL process if you only
+want to import new rows and you get 50 GB with 10 million rows datadumps periodically  with all their data from
+the source system. You could then do except between previous datadump and the
+new one to get these precious 500 new rows that has been added since last time and only import them.
+for this solution to work you need plenty of ram since zet-cmd does all in ram.
+
+## Simple example
+`zet-cmd fileA.csv fileB.csv fileC.csv`
+will to stdoutput show a union of all unique rows. So if two rows are the same
+only one of them will be in the output. Union is the default subcommand.
+
+`zet-cmd intersect --files fileA.csv fileB.csv fileC.csv --output ~/temp/whats-the-same.csv`
+only what rows that are in all of the files will be output to a textfile.
+
+`zet-cmd intersect --files fileA.csv key=1,2,3 ft=csv fileB.csv key=7,8,9  ft=csv fileC.csv key=1,2,3  ft=csv --output ~/temp/whats-the-same.csv`
+only what rows that are all of the files and also we define what fields holds
+the unique key for the row and how the fields are defined and separeted.
+
+## More advanced examples
+`zet-cmd except --files fileA.csv --exceptfile fileB.csv --output ~/temp/whats-new.csv`
+Will show what is in the except file fileb.csv bot not in any of the others
+files which in this case is just fileA.csv
+
+## Really advanced example
+## What parameters does it understand
+
+# How is the program written
+## Testdriven
+## Hashmaps
+## General walk through
+## What libraries does it use
+### Clapper - arguments handler
+### Terminal colors
+
+# Roadmap
+## Goals of this program.
+### What it should be good at.
+Fast, simple
+### What is not should be good at.
+Compete with sql databases
+## Features Request.
+### Sort output file
+### compress large files that has key
+### Guess format of datafiles when you use keys
+
+# How to contribute
+## These are the groundrules
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,76 @@
+// To test this run
+// cargo run -- -o "test.csv" union -f "foo.csv" "fum.csv"
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
     println!("Hello, world!");
+    let matches = App::new("zet-cmder")
+        .version("0.1")
+        .author("Patrik Lindstrom. <patrik.lindstrom.dev>")
+        .about("Extracts data from several textfile with set based logic")
+        .arg(
+            Arg::new("config")
+                .short('c')
+                .long("config")
+                .value_name("CONF")
+                .about("Sets a custom config file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::new("OUTPUT")
+                .about("Sets the a outpuft file for the result")
+                .required(false)
+                .takes_value(true)
+                .short('o')
+                .long("output"),
+        )
+        .arg(
+            Arg::new("v")
+                .short('v')
+                .multiple_occurrences(true)
+                .takes_value(true)
+                .about("Sets the level of verbosity"),
+        )
+        .subcommand(
+            App::new("union")
+                .about("union set operator between files")
+                .version("0.1")
+                .author("Patrik Lindstrom. <patrik.lindstrom.dev>")
+                .arg(
+                    Arg::new("FILES")
+                        .short('f')
+                        .long("files")
+                        .takes_value(true)
+                        .required(true)
+                        .multiple(true)
+                        .min_values(1)
+                        .about("all files that should be in union"),
+                ),
+        )
+        .get_matches();
+    // You can check the value provided by positional arguments, or option arguments
+    if let Some(i) = matches.value_of("OUTPUT") {
+        println!("Value for output: {}", i);
+    }
+    if let Some(c) = matches.value_of("config") {
+        println!("Value for config: {}", c);
+    }
+    match matches.subcommand_name() {
+        Some("union") => println!("'zet-cmder union' was used"),
+        None => println!("No subcommand was used"),
+        _ => println!("Some other subcommand was used"),
+    }
+
+    if let Some(ref matches) = matches.subcommand_matches("union") {
+        // "$ zet-cmder union" was run
+        println!("Subcommand union wasrun");
+        if matches.is_present("FILES") {
+            // "$ myapp union -f filename1.csv filename2.csv" was run
+            let files: Vec<_> = matches.values_of("FILES").unwrap().collect();
+            for f in files {
+                println!("file : {}", f);
+            }
+        }
+    }
 }
