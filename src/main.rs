5cf98a5724a8a10c71fcda42926819bcba3fc610
Patrik Lindström <patrik.lindstrom@lcube.se>
2024-02-26 15:47:53 +0100

    Add difference set operator (#5)

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,14 @@ extern crate clap;
 extern crate csv;
 
 use clap::{load_yaml, App};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use log::{info, debug};
 use env_logger::Builder;
 use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::process;
+
 const KEY_COLUMN: usize = 0;
 const HAS_HEADERS: bool = false;
 
@@ -39,42 +40,67 @@ fn read_data_file(file_path: String, key_column: usize) -> Result<HashMap<String
     Ok(set)
 }
 
+// A or B
 // perform_union should return the zets variable
 fn perform_union(files: Vec<&str>) -> Result<HashMap<String, String>, Box<dyn Error>> {
     // Placeholder for actual union logic
     info!("Performing union operation...");
     let mut zet: HashMap<String, String> = HashMap::new();
     for f in files {
-        let dset = read_data_file(f.to_string(), KEY_COLUMN).expect("Cant handle file");
-        for (key, value) in &dset {
+        info!("Opening file: {}", f);
+        let d_set = read_data_file(f.to_string(), KEY_COLUMN).expect("Cant handle file");
+        for (key, value) in &d_set {
             if !zet.contains_key(key) {
                 zet.insert(key.clone(), value.clone());
             }
         }
         info!("Processed file: {}", f);
     }
+
     info!("Number of Hashmaps are {:?}", zet.len());
     Ok(zet)
 }
 
-/* We first create a counts hashmap to keep track of the count of each key across all files.
+/* A and B
+We first create a counts hashmap to keep track of the count of each key across all files.
  Then, we read each file into the zet hashmap and increment the count of each key in the counts hashmap.
  Finally, we retain only the keys in zet whose count is equal to the number of files.
  This ensures that only the keys that exist in all files are retained.*/
 fn perform_intersect(files: Vec<&str>) -> Result<HashMap<String, String>, Box<dyn Error>> {
     info!("Performing intersect operation...");
+    info!("Current directory file: {}", get_current_dir());
+
     let mut counts: HashMap<String, usize> = HashMap::new();
     let mut zet: HashMap<String, String> = HashMap::new();
     for f in &files {
-        let dset = read_data_file(f.to_string(), KEY_COLUMN).expect("Cant handle file");
-        for (key, value) in dset {
+        info!("Opening file: {}", f);
+        let d_set = read_data_file(f.to_string(), KEY_COLUMN).expect("Cant handle file");
+        for (key, value) in d_set {
             *counts.entry(key.clone()).or_insert(0) += 1;
             zet.insert(key, value);
         }
     }
     zet.retain(|key, _| counts.get(key) == Some(&files.len()));
+
     Ok(zet)
 }
+// not A and B
+fn perform_difference(files: Vec<&str>) -> Result<HashMap<String, String>, Box<dyn Error>> {
+    info!("Performing difference set operation...");
+    let a_file = &files[0];
+    info!("Current directory file: {}", get_current_dir());
+    info!("Opening file: {}", a_file);
+    let mut a_zet: HashMap<String, String> = read_data_file(a_file.to_string(), KEY_COLUMN).expect("Cant handle file");
+    for f in files.iter().skip(1) {
+        println!("Opening file: {}", f);
+        let d_set = read_data_file(f.to_string(), KEY_COLUMN).expect("Cant handle file");
+        let d_keys: HashSet<_> = d_set.keys().map(|k| k.clone()).collect();
+        a_zet.retain(|key, _| !d_keys.contains(key));
+        info!("Processed file: {}", f);
+    }
+    Ok(a_zet)
+}
+
 fn get_current_dir() -> String {
     let path = env::current_dir().unwrap();
     return path.display().to_string();
@@ -112,6 +138,7 @@ fn main() {
                     Ok(zet) => {
                         // Collect the keys into a vector
                         let mut keys: Vec<_> = zet.keys().collect();
+
                         // Sort the keys
                         keys.sort();
                         // Iterate over the sorted keys and print out the corresponding values
@@ -142,8 +169,30 @@ fn main() {
                         process::exit(1);
                     }
                 }
+
+            }
+            ("DIFFERENCE", Some(sub_m)) => {
+                match perform_difference(files.iter().map(AsRef::as_ref).collect()) {
+                    Ok(zet) => {
+                        // Collect the keys into a vector
+                        let mut keys: Vec<_> = zet.keys().collect();
+                        // Sort the keys
+                        keys.sort();
+                        // Iterate over the sorted keys and print out the corresponding values
+                        for key in keys {
+                            println!("Left join result: Key: {:?}, Value: {:?}", key, zet.get(key).unwrap());
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("Error performing Left Join: {}", e);
+                        process::exit(1);
+                    }
+                }
+
             }
             _ => println!("No valid subcommand was used"),
         }
     }
 }
+
+
