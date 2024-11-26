8e46ec21182c6872b2614d488d938513630e886f
Patrik Lindstrom <patrik.lindstrom@lcube.se>
2024-11-26 22:32:33 +0100

    Update dependencies and improve logging in

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
 // To test this run
 // cargo run --  -f "foo.csv" "fum.csv" -o "test.csv" union
 
-// cargo run --  --files "../testdata/foo.csv" "../testdata/fum.csv" -o
-// ../testdata/union_output.csv union
+// cargo run --  --files "../testdata/foo.csv" "../testdata/fum.csv" -o ../testdata/union_output.csv union
 #![allow(unused_variables)]
 
 extern crate csv;
 
-pub mod set_operations;
 pub mod logo;
+pub mod set_operations;
 
-use std::collections::{HashMap};
-use log::{info, debug};
+use crate::set_operations::clap_config::cli;
 use env_logger::Builder;
-use std::error::Error;
-use std::process;
-use set_operations::union::perform_union;
+use log::{debug, info};
+use logo::logo;
 use set_operations::diffa::perform_diffa;
 use set_operations::intersect::perform_intersect;
+use set_operations::union::perform_union;
 use set_operations::xor::perform_xor;
-use logo::logo;
-use crate::set_operations::clap_config::cli;
+use std::collections::HashMap;
+use std::error::Error;
+use std::{env, process};
 
-fn perform_operation(operation: fn(Vec<&String>) -> Result<HashMap<String, String>, Box<dyn Error>>, operation_name: &str, files: Vec<&String>) {
+fn perform_operation(
+    operation: fn(Vec<&String>) -> Result<HashMap<String, String>, Box<dyn Error>>,
+    operation_name: &str,
+    files: Vec<&String>,
+) {
     match operation(files) {
         Ok(zet) => {
             // Collect the keys into a vector
@@ -31,7 +34,12 @@ fn perform_operation(operation: fn(Vec<&String>) -> Result<HashMap<String, Strin
             keys.sort();
             // Iterate over the sorted keys and print out the corresponding values
             for key in keys {
-                println!("{} result: Key: {:?}, Value: {:?}", operation_name, key, zet.get(key).unwrap());
+                println!(
+                    "{} result: Key: {:?}, Value: {:?}",
+                    operation_name,
+                    key,
+                    zet.get(key).unwrap()
+                );
             }
         }
         Err(e) => {
@@ -50,22 +58,27 @@ fn main() {
     // Initialize the logger
     let mut builder = Builder::from_default_env();
     /*    if matches.args_present("debug"){
-            builder.filter_level(log::LevelFilter::Debug);
-        } else {
-            builder.filter_level(log::LevelFilter::Info);
-        }*/
+        builder.filter_level(log::LevelFilter::Debug);
+    } else {
+        builder.filter_level(log::LevelFilter::Info);
+    }*/
     builder.init();
-
+    let cwd = env::current_dir().expect("Failed to get current directory");
+    println!("Current working directory: {}", cwd.display());
     // Now you can use debug! for debug information and info! for always relevant information
     debug!("Debug mode is on.");
     info!("Info mode is on.");
 
     /*    if let Some(i) = matches.value_of("OUTPUT") {
-            println!("Value for output: {}", i);
-        }*/
+        println!("Value for output: {}", i);
+    }*/
     if let Some(files) = matches.get_many::<String>("files") {
         let files_vec: Vec<&String> = files.collect(); // Collect into a Vec<&String>
-        let files_str = files_vec.iter().map(AsRef::as_ref).collect::<Vec<&str>>().join(", ");
+        let files_str = files_vec
+            .iter()
+            .map(AsRef::as_ref)
+            .collect::<Vec<&str>>()
+            .join(", ");
         println!("Files: {}", files_str);
         debug!("Value for files: {}", files_str);
 
@@ -92,5 +105,3 @@ fn main() {
         _ => println!("No valid subcommand was used"),
     }
 }
-
-
--- src/set_operations/clap_config.rs
+++ src/set_operations/clap_config.rs
@@ -1,41 +1,56 @@
-use clap::{Command, Arg,crate_version};
+use clap::{crate_version, Arg, Command};
 pub fn cli() -> Command {
     Command::new("set_operations")
         .version(crate_version!())
         .author("Patrik Lindström <patrik.lindstrom@lcube.se>")
         .about("Performs set operations on csv files")
-        .arg(Arg::new("output")
-            .short('o')
-            .long("output")
-            .value_name("outfile")
-            .help("Sets an output file, stdout if not present")
-            .global(true))
-        .arg(Arg::new("files")
-            .short('f')
-            .long("files")
-            .value_name("file")
-            .help("Sets the input files to use")
-            .required(false)
-            .value_delimiter(',')
-            .global(true))
-        .subcommand(Command::new("union")
-            .about("Performs union operation on csv files")
-            .version("1.0")
-            .author("Patrik Lindström <patrik.lindstrom@lcube.se>"))
-        .subcommand(Command::new("intersect")
-            .about("Performs intersection operation on csv files")
-            .version("1.0")
-            .author("Patrik Lindström <patrik.lindstrom@lcube.se>"))
-        .subcommand(Command::new("diffa")
-            .about("Performs difference operation on csv files")
-            .version("1.0")
-            .author("Patrik Lindström <patrik.lindstrom@lcube.se>"))
-        .subcommand(Command::new("xor")
-            .about("Performs an -exclusive or- operation on csv files")
-            .version("1.0")
-            .author("Patrik Lindström <patrik.lindstrom@lcube.se>"))
-        .subcommand(Command::new("about")
-            .about("Displays the logo")
-            .version("1.0")
-            .author("Patrik Lindström <patrik.lindstrom@lcube.se>"))
+        .arg(
+            Arg::new("output")
+                .short('o')
+                .long("output")
+                .value_name("outfile")
+                .help("Sets an output file, stdout if not present")
+                .global(true),
+        )
+        .arg(
+            Arg::new("files")
+                .short('f')
+                .long("files")
+                .value_name("file")
+                .help("Sets the input files to use")
+                .required(false)
+                .value_delimiter(',')
+                .num_args(1..)
+                .global(true),
+        )
+        .subcommand(
+            Command::new("union")
+                .about("Performs union operation on csv files")
+                .version("1.0")
+                .author("Patrik Lindström <patrik.lindstrom@lcube.se>"),
+        )
+        .subcommand(
+            Command::new("intersect")
+                .about("Performs intersection operation on csv files")
+                .version("1.0")
+                .author("Patrik Lindström <patrik.lindstrom@lcube.se>"),
+        )
+        .subcommand(
+            Command::new("diffa")
+                .about("Performs difference operation on csv files")
+                .version("1.0")
+                .author("Patrik Lindström <patrik.lindstrom@lcube.se>"),
+        )
+        .subcommand(
+            Command::new("xor")
+                .about("Performs an -exclusive or- operation on csv files")
+                .version("1.0")
+                .author("Patrik Lindström <patrik.lindstrom@lcube.se>"),
+        )
+        .subcommand(
+            Command::new("about")
+                .about("Displays the logo")
+                .version("1.0")
+                .author("Patrik Lindström <patrik.lindstrom@lcube.se>"),
+        )
 }
